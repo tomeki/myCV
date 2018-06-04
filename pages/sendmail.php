7583a59154316a5010cbacd8d3a62fcbc1e8fd45
tomeki <tom.brissonnet@gmail.com>
2018-06-05 01:18:21 +0200

    ptite modif

--- pages/sendmail.php
+++ pages/sendmail.php
@@ -1,123 +1,36 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "tom.brissonnet@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
+
 <!DOCTYPE html>
-<!--
-Template Name: Pleeness
-Author: <a href="http://www.os-templates.com/">OS Templates</a>
-Author URI: http://www.os-templates.com/
-Licence: Free to use under our free template licence terms
-Licence URI: http://www.os-templates.com/template-terms
--->
-<!--nom prenom tel mail service site-->
-<html>
 <head>
-<title>Soléa</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
+<title>Form submission</title>
 </head>
-
 <body>
-<form class="form-horizontal" action="sendmail.php" method="post"  id="contact_form" enctype="text/plain">
-	<!-- Text input-->
-		<div class="form-group">
-			<label class="col-md-4 control-label">Nom</label>
-			<div class="col-md-6  inputGroupContainer">
-				<div class="input-group"> <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-					<input  name="nom" placeholder="Nom" class="form-control"  type="text">
-				</div>
-			</div>
-		</div>
 
-		<!-- Text input-->
-
-		<div class="form-group">
-			<label class="col-md-4 control-label" >Prénom</label>
-			<div class="col-md-6  inputGroupContainer">
-				<div class="input-group"> <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-					<input name="prenom" placeholder="Prénom" class="form-control"  type="text">
-				</div>
-			</div>
-		</div>
-
-
-		<!-- Text input-->
-		<div class="form-group">
-			<label class="col-md-4 control-label">E-Mail</label>
-			<div class="col-md-6  inputGroupContainer">
-				<div class="input-group"> <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-					<input name="email" placeholder="Adresse E-mail" class="form-control"  type="email">
-				</div>
-			</div>
-		</div>
-		<!-- Text input-->
-		<div class="form-group">
-			<label class="col-md-4 control-label">Sujet</label>
-			<div class="col-md-6  inputGroupContainer">
-				<input name="sujet" placeholder="Sujet" class="form-control"  type="text">
-			</div>
-		</div>
-		<!-- Text input-->
-		<div class="form-group">
-			<label class="col-md-4 control-label">Message</label>
-			<div class="col-md-6  inputGroupContainer">
-				<textarea rows="5" name="message" placeholder="Message" class="form-control"></textarea>
-			</div>
-		</div>
-		<!-- Button -->
-		<div class="form-group">
-			<label class="col-md-4 control-label"></label>
-			<div class="col-md-4">
-				<button name="envoyer" type="submit" class="btn btn-warning" >Envoyer <span class="glyphicon glyphicon-send"></span></button>
-			</div>
-		</div>	
+<form action="" method="post">
+First Name: <input type="text" name="first_name"><br>
+Last Name: <input type="text" name="last_name"><br>
+Email: <input type="text" name="email"><br>
+Message:<br><textarea rows="5" name="message" cols="30"></textarea><br>
+<input type="submit" name="submit" value="Submit">
 </form>
 
-<!-- Sending message-->
-<?php 
-if(isset($_POST["send"])){
-echo "<h2 id='titre'>A message has been sent : </h2>";
-
-$mail = 'thomas.brissonnet@master-stic.fr';
-$boundary = "-----=".md5(rand());
-/*Space in mail*/
-if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) {
-$passage_ligne = "\r\n";
-}
-else {
-$passage_ligne = "\n";
-}
-
-
-/*Open Boundary*/
-$message = "...";
-
-/*Header Mail*/
-$header = "From: \"EXPEDITEUR\"<".$_POST["email"].">".$passage_ligne;
-$header.= "Reply-to: \"RETOUR\" <".$mail.">".$passage_ligne;
-$header.= "MIME-Version: 1.0".$passage_ligne;
-$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
-
-$message = $passage_ligne."--".$boundary.$passage_ligne;
-
-/*Mail in Text*/
-$message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
-$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-$message.= $passage_ligne.$_POST["gdtext"].$passage_ligne;
-$message.= $passage_ligne."--".$boundary.$passage_ligne;
-
-$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-
-
-/*Sending mail*/
-mail($mail,$_POST["subject"],$message,$header);
-
-/*Display mail information*/
-echo 'Your mail address : '. $_POST["email"]."<br/>";
-echo 'Subject : ' . $_POST["sujet"]."<br/>";
-echo 'Content : ' . $_POST["message"];
-}
-?>
-		   
 </body>
-</html>
+</html> <strong></strong>
