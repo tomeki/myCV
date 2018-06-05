28d72cd54b99b0d1f991c812f195618efbd1bbdd
tomeki <tom.brissonnet@gmail.com>
2018-06-05 17:33:04 +0200

    ajout du htaccess

--- static/include/footer.php
+++ static/include/footer.php
@@ -1,7 +1,7 @@
 <div class="footer">
 	<nav class="navbar navbar-default navbar-fixed-bottom">
 		<div class="container footer_center">
-			<p class="navbar-text ">Thomas Brissonnet &copy; 2018 • Tous droits réservés • <a class="footer-link" href="contact.php">Contact</a></p>
+			<p class="navbar-text ">Thomas Brissonnet &copy; 2018 • Tous droits réservés • <a class="footer-link" href="contact">Contact</a></p>
 		</div>
 	</nav>
 </div>
