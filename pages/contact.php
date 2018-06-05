a596c0e136a1c44d92e5b15753da8515f8c38b9d
tomeki <tom.brissonnet@gmail.com>
2018-06-05 04:02:22 +0200

    ptite modif

--- pages/contact.php
+++ pages/contact.php
@@ -68,7 +68,7 @@
 		<div class="panel panel-default">
 			<div class="panel-heading">Me contacter</div>
 				<div class="panel-body">
-				    <form class="form-horizontal" action="" method="post"  id="contact_form">
+				    <form class="form-horizontal" action="confirm.php" method="post"  id="contact_form">
 						<!-- Text input-->
 							<div class="form-group">
 								<label class="col-md-4 control-label">Nom</label>
@@ -125,45 +125,6 @@
 				</div>
 			</div>
 <?php 
-if(isset($_POST['envoyer'])){
-$mail = 'thomas.brissonnet@tbrissonnet.ovh';
-$boundary = "-----=".md5(rand());
-/*Space in mail*/
-if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) {
-	$passage_ligne = "\r\n";
-}
-else {
-	$passage_ligne = "\n";
-}				
-/*Open Boundary*/
-$message = "...";
-
-/*Header Mail*/
-$header = "From: \"EXPEDITEUR\"<".$_POST["email"].">".$passage_ligne;
-$header.= "Reply-to: \"RETOUR\" <".$mail.">".$passage_ligne;
-$header.= "MIME-Version: 1.0".$passage_ligne;
-$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
-
-$message = $passage_ligne."--".$boundary.$passage_ligne;
-
-/*Mail in Text*/
-$message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
-$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-$message.= $passage_ligne.$_POST["message"].$passage_ligne;
-$message.= $passage_ligne."--".$boundary.$passage_ligne;
-
-$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-
-
-/*Sending mail*/
-$test = mail($mail,$_POST["sujet"],$message,$header);
-echo $test."<br/>";
-
-/*Display mail information*/
-echo 'Your mail address : '. $_POST["email"]."<br/>";
-echo 'Subject : ' . $_POST["sujet"]."<br/>";
-echo 'Content : ' . $_POST["message"];
 }
 ?>
 	</div>
