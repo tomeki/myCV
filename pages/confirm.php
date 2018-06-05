3efa9efeff202f45eb35eeed272984b49f0cb169
tomeki <tom.brissonnet@gmail.com>
2018-06-05 04:08:13 +0200

    finalisation du formulaire d'envoie de message

--- pages/confirm.php
+++ pages/confirm.php
@@ -45,47 +45,49 @@
 	</nav>
 	<!-- Content-->
 	<div class="container">
-<?php if(isset($_POST['envoyer'])){
-$mail = 'thomas.brissonnet@tbrissonnet.ovh';
-$boundary = "-----=".md5(rand());
-/*Space in mail*/
-if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) {
-	$passage_ligne = "\r\n";
-}
-else {
-	$passage_ligne = "\n";
-}				
-/*Open Boundary*/
-$message = "...";
+<?php 
+	if(isset($_POST['envoyer'])){
+		$mail = 'thomas.brissonnet@tbrissonnet.ovh';
+		$boundary = "-----=".md5(rand());
+		/*Space in mail*/
+		if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) {
+			$passage_ligne = "\r\n";
+		}
+		else {
+			$passage_ligne = "\n";
+		}				
+		/*Open Boundary*/
+		$message = "...";
 
-/*Header Mail*/
-$header = "From: \"EXPEDITEUR\"<".$_POST["email"].">".$passage_ligne;
-$header.= "Reply-to: \"RETOUR\" <".$mail.">".$passage_ligne;
-$header.= "MIME-Version: 1.0".$passage_ligne;
-$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+		/*Header Mail*/
+		$header = "From: \"EXPEDITEUR\"<".$_POST["email"].">".$passage_ligne;
+		$header.= "Reply-to: \"RETOUR\" <".$mail.">".$passage_ligne;
+		$header.= "MIME-Version: 1.0".$passage_ligne;
+		$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
 
-$message = $passage_ligne."--".$boundary.$passage_ligne;
+		$message = $passage_ligne."--".$boundary.$passage_ligne;
 
-/*Mail in Text*/
-$message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
-$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-$message.= $passage_ligne.$_POST["message"].$passage_ligne;
-$message.= $passage_ligne."--".$boundary.$passage_ligne;
+		/*Mail in Text*/
+		$message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
+		$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+		$message.= $passage_ligne.$_POST["message"].$passage_ligne;
+		$message.= $passage_ligne."--".$boundary.$passage_ligne;
 
-$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+		$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+		$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
 
 
-/*Sending mail*/
-$test = mail($mail,$_POST["sujet"],$message,$header);
-if ($test==1){
-	echo"<div class='alert alert-info' role='alert'>
+		/*Sending mail*/
+		$test = mail($mail,$_POST["sujet"],$message,$header);
+		if ($test==1){
+			echo"<div class='alert alert-info' role='alert'>
 			Message envoyé avec succès!
-		</div> ";
-}else{
-	echo"<div class='alert alert-warning' role='alert'>
-		Problème d'envoie du message!
-	</div>";
+			</div> ";
+		}else{
+			echo"<div class='alert alert-warning' role='alert'>
+			Problème d'envoie du message!
+			</div>";
+		}
 }
 ?>
 
