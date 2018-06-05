832a02263747e6ac2bbcff4f39c67ca8db12a3b3
tomeki <tom.brissonnet@gmail.com>
2018-06-05 05:37:35 +0200

    changement de chemin

--- static/include/footer.php
+++ static/include/footer.php
@@ -1,4 +1,4 @@
-<div class="footer">
+<div class="footer">gfdg
 	<nav class="navbar navbar-default navbar-fixed-bottom">
 		<div class="container">
 			<p class="navbar-text ">Thomas Brissonnet &copy; 2018 • Tous droits réservés • <a class="footer-link" href="contact.php">Contact</a></p>
