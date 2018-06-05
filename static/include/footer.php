37e93f22558645b6ae878daf193c952f4bf32ea5
tomeki <tom.brissonnet@gmail.com>
2018-06-05 06:06:32 +0200

    changes

--- static/include/footer.php
+++ static/include/footer.php
@@ -1,7 +1,6 @@
-<div class="footer">
-	<nav class="navbar navbar-default navbar-fixed-bottom">
+
+<nav class="navbar navbar-default navbar-fixed-bottom footer">
 		<div class="container footer_center">
 			<p class="navbar-text ">Thomas Brissonnet &copy; 2018 • Tous droits réservés • <a class="footer-link" href="contact.php">Contact</a></p>
 		</div>
-	</nav>
-</div>
+</nav>
